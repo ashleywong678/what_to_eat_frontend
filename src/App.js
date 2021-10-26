6c518ed75e756bd959db30c75e78387c0edc597b
Ashley <ashleywong678@gmail.com>
2021-10-26 12:05:23 -0400

    error handling for login/signup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom';
+
+import LoginPage from './LoginPage/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [token, setToken] = useState('');
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>What to Eat</h1>
+			</header>
+			<Router>
+				<Switch>
+					<Route path='/'>
+						{token ? (
+							<Redirect to='/recipes' />
+						) : (
+							<LoginPage
+								setCredentials={(value) => {
+									setToken(value.token);
+									return <Redirect to='/main' />;
+								}}
+							/>
+						)}
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/LoginPage/index.js
@@ -0,0 +1,89 @@
+import './login.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const validTypes = { login: 'Login', signup: 'Sign-up' };
+const defaultForm = { username: '', password: '' };
+
+const LoginPage = ({ setCredentials, status }) => {
+	const [type, setType] = useState('login');
+	const [form, setForm] = useState(defaultForm);
+	const [error, setError] = useState('');
+
+	const setValue = (key, value) => {
+		const newForm = { ...form, [key]: value };
+		setForm(newForm);
+	};
+
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+
+		if (type === 'login') {
+			await axios
+				.post(`http://localhost:3001/api/log_in`, form)
+				.then((res) => res.data)
+				.then((res) => {
+					setCredentials(res);
+					setError('');
+				})
+				.catch((err) => {
+					if (err.response && err.response.data) setError(err.response.data);
+				});
+		} else {
+			await axios
+				.post(`http://localhost:3001/api/sign_up`, form)
+				.then((res) => res.data)
+				.then((res) => setCredentials(res))
+				.catch((err) => {
+					if (err.response && err.response.data) setError(err.response.data);
+				});
+		}
+	};
+
+	return (
+		<>
+			<h1 className='login-title'>{validTypes[type]}</h1>
+			<form onSubmit={handleFormSubmit}>
+				<label htmlFor='username'>Username:</label>
+				<input
+					style={{ margin: '5px' }}
+					type='username'
+					name='username'
+					id='username'
+					required
+					value={form.username}
+					onChange={({ target }) => setValue('username', target.value)}
+				/>
+				<label htmlFor='password'>Password:</label>
+				<input
+					style={{ margin: '5px' }}
+					type='password'
+					name='password'
+					id='password'
+					required
+					value={form.password}
+					onChange={({ target }) => setValue('password', target.value)}
+				/>
+				<button type='submit'>{validTypes[type]}</button>
+				{error ? <p id='login-error'>{error}</p> : null}
+			</form>
+			<button
+				type='button'
+				onClick={() => {
+					setType(type === 'login' ? 'signup' : 'login');
+					setError('');
+				}}
+			>
+				{type === 'login' ? 'Sign-up' : 'Login'}
+			</button>
+		</>
+	);
+};
+
+LoginPage.propTypes = {
+	setCredentials: PropTypes.func,
+	status: PropTypes.string,
+};
+
+export default LoginPage;
