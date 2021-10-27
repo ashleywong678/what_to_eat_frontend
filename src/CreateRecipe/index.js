31c4b93ea46626c5158cce38d466b5bdf31bbf89
Ashley <ashleywong678@gmail.com>
2021-10-27 16:13:57 -0400

    init commit

---
+++ src/CreateRecipe/index.js
@@ -0,0 +1,14 @@
+import './createRecipe.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const CreateRecipe = ({ token }) => {
+	return <div>Create</div>;
+};
+
+CreateRecipe.propTypes = {
+	token: PropTypes.string,
+};
+
+export default CreateRecipe;
